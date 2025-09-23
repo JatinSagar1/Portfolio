4ad0bb3f2b8b554ac3d0d7fc46adcc9ee3475b4e
JatinSagar1 <jjatinsagar@gmail.com>
2025-09-23 18:21:01 +0530

    experiement

--- src/components/SocialSidebar.jsx
+++ src/components/SocialSidebar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+// import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 import "../css/SocialSidebar.css";
 
 export default function SocialSidebar() {
   return (
     <div className="social-sidebar">
-      <a href="https://github.com/JatinSagar1" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
-      <a href="https://linkedin.com/in/jatin-sagar-99025922a" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
-      <a href="https://x.com/jjatinsagar" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
+      <a href="https://github.com/JatinSagar1" target="_blank" rel="noopener noreferrer">Github</a>
+      <a href="https://linkedin.com/in/jatin-sagar-99025922a" target="_blank" rel="noopener noreferrer">Link</a>
+      <a href="https://x.com/jjatinsagar" target="_blank" rel="noopener noreferrer">Twitt</a>
     </div>
   );
 }
